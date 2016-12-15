bf044cd7e7c4116042e62a1b1f0bf252b0e82f92
unknown <>
2016-12-15 17:10:42 +0800

    钉钉微应用

---
+++ 钉钉项目/README.md
@@ -0,0 +1,7 @@
+# simple jsapi demo
+
+## awesome project
+
+### Getting started
+1. set up your own corp id and corp secret in env.js
+2. public/javascript/demo.js rocks!
---
+++ 钉钉项目/public/javascripts/demo.js
@@ -0,0 +1,43 @@
+/**
+ * Created by liqiao on 8/10/15.
+ */
+
+/**
+ * _config comes from server-side template. see views/index.jade
+ */
+dd.config({
+    agentId: '54989834',
+    corpId: _config.corpId,
+    timeStamp: _config.timeStamp,
+    nonceStr: _config.nonceStr,
+    signature: _config.signature,
+    jsApiList: ['device.notification.confirm',
+        'device.notification.alert',
+        'device.notification.prompt',
+        'biz.chat.chooseConversation',
+        'biz.ding.post']
+});
+
+dd.ready(function() {
+    alert('dd ready');
+
+
+    var head = document.querySelector('h1');
+    head.innerHTML = head.innerHTML + ' It rocks!';
+
+    dd.device.notification.alert({
+        message: 'dd.device.notification.alert',
+        title: 'This is title',
+        buttonName: 'button',
+        onSuccess: function(data) {
+            alert('win: ' + JSON.stringify(data));
+        },
+        onFail: function(err) {
+            alert('fail: ' + JSON.stringify(err));
+        }
+    });
+});
+
+dd.error(function(err) {
+    alert('dd error: ' + JSON.stringify(err));
+});
